504d975b5a7b53c0c50d24f82f486acf3fc51a0e
Dark Cyber <87921701+DarkCybers@users.noreply.github.com>
2021-08-21 01:09:14 +0530

    Update innexia.py

--- innexia.py
+++ innexia.py
@@ -12,7 +12,7 @@ bot_token= BOT_TOKEN
 print("[INFO]: Checking... Your Details")
 
 bot_id = int(bot_token.split(":")[0])
-print("[INFO]: Code running by master Blaze")
+print("[INFO]: Code running by master Blaze and Sammy")
 arq = None
 
 
